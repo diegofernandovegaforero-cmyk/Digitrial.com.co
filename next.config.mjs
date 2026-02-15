a9309a279a505ee3b91f675076dc92a6210e9d4d
diegofernandovegaforero-cmyk <diegofernandovegaforero@gmail.com>
2026-02-15 12:25:06 -0500

    Fix next.config.mjs incompatible options

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
     },
     typescript: {
         ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
     },
 };
 
